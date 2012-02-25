91c264a5054d25f5d89aeed1e8e8990ad47d4804
Colin Thomas-Arnold <colinta@mac.com>
2012-02-24 22:57:26 -0700

    brought it up to date with most recent strange case... now, whether to move extensions/ into strange case or not...

--- config.py
+++ config.py
@@ -1,14 +1,7 @@
-from strange_case_jinja import StrangeCaseEnvironment
-from extensions.markdown2_extension import Markdown2Extension, markdown2
-from extensions.date_extension import date
-from time import time
-from datetime import datetime
-
-ENVIRONMENT = StrangeCaseEnvironment(extensions=[Markdown2Extension])
-ENVIRONMENT.filters['date'] = date
-ENVIRONMENT.filters['markdown2'] = markdown2
+from strange_case_config import CONFIG
+from datetime import date
 
 
-CONFIG = {
-        'date': datetime.date.today(),
-        }
+CONFIG.update({
+    'date': date.today(),
+})
